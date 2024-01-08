657fbb4645723baa5daddc90dd7f4da519db79f6
4Nameless4 <623507166@qq.com>
2024-01-08 22:36:30 +0800

    optimizing

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function isType(val, type) {
 function isFunction(val) {
   return isType(val, "function");
 }
-function isObject(val) {
-  return isType(val, "object");
+function isObjectOrFunction(val) {
+  return isType(val, "object") || isFunction(val);
 }
 function isPending(state) {
   return state === states.PENDING;
@@ -24,7 +24,7 @@ function onFulfilledDefault(val) {
 function onRejectedDefault(e) {
   throw e;
 }
-let count = 1;
+
 function Nlpromise(executor) {
   var that = this;
   if (!(that instanceof Nlpromise)) {
@@ -33,117 +33,78 @@ function Nlpromise(executor) {
     );
   }
 
-  that.id = count++;
-
   that.state = states.PENDING;
   that.result = undefined;
-  that.queue = [];
+  var queue = [];
 
-  function resolve(value) {
+  function exe(value, state) {
     if (!isPending(that.state)) return;
-    that.state = states.FULFILLED;
+    that.state = state;
     that.result = value;
     next();
+  }
+  function resolve(value) {
+    exe(value, states.FULFILLED);
   }
   function reject(value) {
-    if (!isPending(that.state)) return;
-    that.state = states.REJECTED;
-    that.result = value;
-    next();
+    exe(value, states.REJECTED);
   }
 
-  function _next_fun(
-    value,
-    nextResolve,
-    nextReject,
-    nextPromise,
-    type = false
-  ) {
-    function _() {
-      try {
-        var called = false;
-        var then = null;
-        if (value && nextPromise === value) {
-          throw new TypeError("Chaining cycle");
-        } else if (
-          (isObject(value) || isFunction(value)) &&
-          (then = value.then) &&
-          isFunction(then)
-        ) {
-          then.call(
-            value,
-            (v) => {
-              if (called) return;
-              called = true;
-              _next_fun(v, nextResolve, nextReject, nextPromise, true);
-            },
-            (v) => {
-              if (called) return;
-              called = true;
-              nextReject(v);
-            }
-          );
-        } else {
-          nextResolve(value);
-        }
-      } catch (e) {
-        if (called) return;
-        nextReject(e);
+  function _next(getValue, queue_next) {
+    try {
+      var nextResolve = queue_next.nextResolve;
+      var nextReject = queue_next.nextReject;
+      var nextPromise = queue_next.nextPromise;
+      var value = getValue();
+      // called -> {then:(res)=>{res("something");throw "error"}}
+      var called = false;
+      var then = null;
+      if (value && nextPromise === value) {
+        throw new TypeError("Chaining cycle");
+      } else if (
+        isObjectOrFunction(value) &&
+        (then = value.then) &&
+        isFunction(then)
+      ) {
+        then.call(
+          value,
+          (v) => {
+            if (called) return;
+            called = true;
+            // _next -> {then:(res)=>{res({then:(rej)=>rej("hh")});}}
+            setTimeout(() => _next(() => v, queue_next));
+          },
+          (v) => {
+            if (called) return;
+            called = true;
+            nextReject(v);
+          }
+        );
+      } else {
+        nextResolve(value);
       }
-    }
-    if (type) {
-      setTimeout(_);
-    } else {
-      _();
+    } catch (e) {
+      if (called) return;
+      nextReject(e);
     }
   }
 
   function next() {
-    if (isPending(that.state) || that.queue.length <= 0) return;
+    if (isPending(that.state) || queue.length <= 0) return;
     setTimeout(() => {
-      while (that.queue.length) {
-        var _next = that.queue.shift();
+      while (queue.length) {
+        var queue_next = queue.shift();
 
-        var call =
-          that.state === states.FULFILLED
-            ? _next.onFulfilled
-            : _next.onRejected;
-        var nextResolve = _next.nextResolve;
-        var nextReject = _next.nextReject;
-        var nextPromise = _next.nextPromise;
-
-        try {
-          //   if (call === null) {
-          //     that.state === states.FULFILLED
-          //       ? nextResolve(that.result)
-          //       : nextReject(that.result);
-          //   }
-          var value = call(that.result);
-          _next_fun(value, nextResolve, nextReject, nextPromise);
-          //   var then = null;
-
-          //   if (value && nextPromise === value) {
-          //     throw new TypeError("Chaining cycle");
-          //   } else if (
-          //     (isObject(value) || isFunction(value)) &&
-          //     (then = value.then) &&
-          //     isFunction(then)
-          //   ) {
-          //     then.call(value, nextResolve, nextReject);
-          //   } else {
-          //     nextResolve(value);
-          //   }
-        } catch (e) {
-          nextReject(e);
-        }
+        var call = queue_next[that.state];
+        _next(() => call(that.result), queue_next);
       }
     });
   }
 
   function then(onFulfilled, onRejected, nextResolve, nextReject, nextPromise) {
-    that.queue.push({
-      onFulfilled,
-      onRejected,
+    queue.push({
+      [states.FULFILLED]: onFulfilled,
+      [states.REJECTED]: onRejected,
       nextResolve,
       nextReject,
       nextPromise,
@@ -174,51 +135,3 @@ Nlpromise.prototype.then = function (onFulfilled, onRejected) {
 };
 
 module.exports = Nlpromise;
-
-const sentinel = { sentinel: "sentinel" };
-
-var p1 = new Promise((res) => res("1"));
-var p2 = p1.then(function () {
-  console.log("1");
-  return {
-    then: function (onFulfilled) {
-      console.log("2");
-      onFulfilled({
-        then: function (onFulfilled) {
-          console.log("3");
-          onFulfilled(sentinel);
-          console.log("3-1");
-          throw "hhhh";
-        },
-      });
-      console.log("2-1");
-    },
-  };
-});
-var p3 = p2.then(function (d) {
-  console.log("done", d === sentinel);
-});
-
-p3.then(() => {
-  var p1 = new Nlpromise((res) => res("1"));
-  var p2 = p1.then(function () {
-    console.log("1");
-    return {
-      then: function (onFulfilled) {
-        console.log("2");
-        onFulfilled({
-          then: function (onFulfilled) {
-            console.log("3");
-            onFulfilled(sentinel);
-            console.log("3-1");
-            throw "hhhh";
-          },
-        });
-        console.log("2-1");
-      },
-    };
-  });
-  var p3 = p2.then(function (d) {
-    console.log("done", d === sentinel);
-  });
-});
